162d5722479230e1effdebfa2dd54f8a27124703
Diogo-poke <diogopda2007@gmail.com>
2025-09-30 22:29:14 -0300

    Util

--- Models/Personagem.cs
+++ Models/Personagem.cs
@@ -21,6 +21,12 @@ namespace RpgApi.Models
         public int Inteligencia { get; set; }
 
         public ClasseEnum Classe { get; set; }
+
+        public byte[]? FotoPersonagem {get ; set;}
+
+        public int? UsuarioId {get; set;}
+
+        public Usuario? Usuario {get; set;}
     }
 
 }
